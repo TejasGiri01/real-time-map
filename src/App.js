f496a18aec46eb78d026290006b39d32356c4d17
Tejas giri <tejas.gosavi0001@gmail.com>
2025-02-28 15:44:25 +0530

    Deploy frontend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React, { useState } from "react";
 import MapComponent from "./components/MapComponent";
 import LocationForm from "./components/LocationForm";
+import "./App.css"; // Import CSS
 
 function App() {
   const [initialPosition, setInitialPosition] = useState([51.505, -0.09]); // Default location
   const [destination, setDestination] = useState([51.515, -0.1]); // Prevents null errors
 
   return (
-    <div>
+    <div className="app-container">
       <h1>Real-Time Location Map</h1>
-      <LocationForm 
-        setInitialPosition={setInitialPosition} 
-        setDestination={setDestination} 
+      <LocationForm
+        setInitialPosition={setInitialPosition}
+        setDestination={setDestination}
       />
-      <MapComponent 
-        initialPosition={initialPosition} 
-        destination={destination} 
-      />
+      <MapComponent initialPosition={initialPosition} destination={destination} />
     </div>
   );
 }
